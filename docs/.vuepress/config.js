2d11987f178c9099309bbf6533afba1c1c4e72ac
许剑煜 <806390502@qq.com>
2020-01-08 14:09:24 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,6 +15,7 @@ module.exports = {
     lineNumbers: false // 代码块显示行号
   },
   themeConfig: {
+    extend: '@vuepress/theme-default',
     logo: '/logo.jpg',
     lastUpdated: 'lastUpdate', // string | boolean
     nav: [
