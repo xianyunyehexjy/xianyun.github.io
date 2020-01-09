d89e9ac8ae64e4a28dfec0c0dd1f79538ca5ebe1
许剑煜 <806390502@qq.com>
2020-01-09 10:15:41 +0800

    修改

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -26,9 +26,11 @@
         <slot name="sidebar-bottom" />
       </template>
     </Sidebar>
-
+<div v-if="flag">
+      <aplayer :audio="audio" :lrcType="3" fixed />
+     </div>
     <Home v-if="$page.frontmatter.home" />
-
+     
     <Page
       v-else
       :sidebar-items="sidebarItems"
@@ -50,6 +52,8 @@ import Page from '@theme/components/Page.vue'
 import Sidebar from '@theme/components/Sidebar.vue'
 import { resolveSidebarItems } from '../util'
 
+import axios from 'axios'
+import APlayer from '@moefe/vue-aplayer';
 export default {
   name: 'Layout',
 
@@ -57,12 +61,37 @@ export default {
     Home,
     Page,
     Sidebar,
-    Navbar
+    Navbar,
+    APlayer
   },
 
   data () {
     return {
-      isSidebarOpen: false
+      isSidebarOpen: false,
+      flag:false,
+      audio: [
+          {
+            name: '东西（Cover：林俊呈）',
+            artist: '纳豆',
+            url: 'https://cdn.moefe.org/music/mp3/thing.mp3',
+            cover: 'https://p1.music.126.net/5zs7IvmLv7KahY3BFzUmrg==/109951163635241613.jpg?param=300y300', // prettier-ignore
+            lrc: 'https://cdn.moefe.org/music/lrc/thing.lrc',
+          },
+          {
+            name: '响喜乱舞（Cover：MARiA）',
+            artist: '泠鸢yousa',
+            url: 'https://cdn.moefe.org/music/mp3/kyoukiranbu.mp3',
+            cover: 'https://p1.music.126.net/AUGVPQ_rVrngDH9ocQrn3Q==/109951163613037822.jpg?param=300y300', // prettier-ignore
+            lrc: 'https://cdn.moefe.org/music/lrc/kyoukiranbu.lrc',
+          },
+          {
+            name: '啵唧',
+            artist: 'Hanser',
+            url: 'https://cdn.moefe.org/music/mp3/kiss.mp3',
+            cover: 'https://p1.music.126.net/K0-IPcIQ9QFvA0jXTBqoWQ==/109951163636756693.jpg?param=300y300', // prettier-ignore
+            lrc: 'https://cdn.moefe.org/music/lrc/kiss.lrc',
+          }
+        ],
     }
   },
 
@@ -119,9 +148,18 @@ export default {
     this.$router.afterEach(() => {
       this.isSidebarOpen = false
     })
+    this.init()
   },
 
   methods: {
+    async init () {
+        //这边是引入了axios然后使用的get请求的一个音乐列表接口
+        const getMusicList = url => axios.get(url);
+        let url = 'https://api.i-meto.com/meting/api?server=netease&type=playlist&id=2200340584&r=random';
+        let data = await getMusicList(url);
+        this.audio=data.data
+        this.flag = true; 
+      },
     toggleSidebar (to) {
       this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
       this.$emit('toggle-sidebar', this.isSidebarOpen)
