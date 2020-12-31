6b3bacce71673fa21125bc7c7fe53113f381ccab
许剑煜 <806390502@qq.com>
2020-12-31 17:04:37 +0800

    Update enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,12 +1,14 @@
-import type from './components/type'
-import heart from './components/heart'
+import type from './components/type';
+import heart from './components/heart';
+import year from './components/year';
 export default ({
-    Vue, // VuePress 正在使用的 Vue 构造函数
-    options, // 附加到根实例的一些选项
-    router, // 当前应用的路由实例
-    siteData // 站点元数据
-  }) => {
-    // ...做一些其他的应用级别的优化
-    Vue.component('type',type)
-    Vue.component('heart',heart)
-  }
+	Vue, // VuePress 正在使用的 Vue 构造函数
+	options, // 附加到根实例的一些选项
+	router, // 当前应用的路由实例
+	siteData, // 站点元数据
+}) => {
+	// ...做一些其他的应用级别的优化
+	Vue.component('type', type);
+	Vue.component('heart', heart);
+	Vue.component('year', year);
+};
