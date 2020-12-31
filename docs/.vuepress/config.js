6d3d93aa142949e8efe0e955ee5afb18af8a18b5
xujy <806390502@qq.com>
2020-12-31 17:18:34 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,54 +1,60 @@
 module.exports = {
-  title: '闲云首页',
-  description: '本是青灯不归客，却因浊酒恋红尘。',
-  head: [  
-    ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
-    ["meta", {name: "robots", content: "all"}],
-    ["meta", {name: "author", content: "许剑煜"}],
-    ["meta", {name: "keywords", content: "Vue笔记"}],
-    ['link', { rel: 'stylesheet', href: '/live2dModels/live2d/css/common.css' }],  //引入css
-    ['link', { rel: 'stylesheet', href: '/css/APlayer.min.css' }],  //引入css
-    ['script', { type: 'text/javascript', src: '/live2dModels/live2d/js/live2d.js' }],  //引入live2d
-    ['script', { type: 'text/javascript', src: '/live2dModels/live2d/js/init.js' }] , //引入live2d初始化脚本,async:'async'
-    ['script', { type: 'text/javascript', src: '/js/APlayer.min.js' }] ,
-  ],
-  base: '/xianyun.github.io/', // 这是部署到github相关的配置
-  markdown: {
-    lineNumbers: false // 代码块显示行号
-  },
-  themeConfig: {
-    logo: '/logo.jpg',
-    lastUpdated: 'lastUpdate', // string | boolean
-    nav: [
-      { text: '首页', link: '/' },
-      {
-          text: '归档',
-          ariaLabel: '归档',
-          items: [
-              { text: '文章', link: '/Vue/' },
-              { text: '琐碎', link: '/Other/' },
-          ]
-      },
-      {text: '图集', link: '/Pictures/' },
-      {text: 'GitHub', link: 'https://github.com/xianyunyehexjy'}     
-  ],
-    sidebar: 'auto', // 侧边栏配置
-    sidebarDepth: 2, // 侧边栏显示2级
-  },
-  plugins: [
-    '@vuepress/pwa',
-    'vuepress-plugin-medium-zoom',
-      {
-        selector: 'img',
-        delay: 1000,
-        options: {
-          scrollOffset: 0,
-        },
-      }
-      
-  ],
-  globalUIComponents: [
-    'player',
-    'card'
-  ]
+	title: '闲云首页',
+	description: '本是青灯不归客，却因浊酒恋红尘。',
+	head: [
+		['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+		['meta', { name: 'robots', content: 'all' }],
+		['meta', { name: 'author', content: '许剑煜' }],
+		['meta', { name: 'keywords', content: 'Vue笔记' }],
+		[
+			'link',
+			{ rel: 'stylesheet', href: '/live2dModels/live2d/css/common.css' },
+		], //引入css
+		['link', { rel: 'stylesheet', href: '/css/APlayer.min.css' }], //引入css
+		[
+			'script',
+			{ type: 'text/javascript', src: '/live2dModels/live2d/js/live2d.js' },
+		], //引入live2d
+		[
+			'script',
+			{ type: 'text/javascript', src: '/live2dModels/live2d/js/init.js' },
+		], //引入live2d初始化脚本,async:'async'
+		['script', { type: 'text/javascript', src: '/js/APlayer.min.js' }],
+	],
+	base: '/xianyun.github.io/', // 这是部署到github相关的配置
+	markdown: {
+		lineNumbers: false, // 代码块显示行号
+	},
+	themeConfig: {
+		logo: '/logo.jpg',
+		lastUpdated: 'lastUpdate', // string | boolean
+		nav: [
+			{ text: '首页', link: '/' },
+			{
+				text: '归档',
+				ariaLabel: '归档',
+				items: [
+					{ text: '文章', link: '/Vue/' },
+					{ text: '琐碎', link: '/Other/' },
+				],
+			},
+			{ text: '图集', link: '/Pictures/' },
+			{ text: '新年快乐', link: '/Year/' },
+			{ text: 'GitHub', link: 'https://github.com/xianyunyehexjy' },
+		],
+		sidebar: 'auto', // 侧边栏配置
+		sidebarDepth: 2, // 侧边栏显示2级
+	},
+	plugins: [
+		'@vuepress/pwa',
+		'vuepress-plugin-medium-zoom',
+		{
+			selector: 'img',
+			delay: 1000,
+			options: {
+				scrollOffset: 0,
+			},
+		},
+	],
+	globalUIComponents: ['player', 'card'],
 };
--- docs/README.md
+++ docs/README.md
@@ -2,8 +2,8 @@
 home: true
 heroText: 许剑煜的前端记录
 ---
+
 <header class="hero">
-<img src="https://api.ixiaowai.cn/mcapi/mcapi.php" alt="hero">
+<!-- <img src="https://api.ixiaowai.cn/mcapi/mcapi.php" alt="hero"> -->
 <type/>
 </header>
- 
